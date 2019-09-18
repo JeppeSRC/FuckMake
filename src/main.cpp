ab8e0230ac9ea6ab93d1a0419a901c015e6ac80e
JeppeSRC <jeppe97rc@gmail.com>
2019-09-18 09:05:22 +0200

    Minor change

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,12 @@
 int main(int argc, char** argv) {
 	String target("__default__");
 
-	if (argc == 2) {
+	if (argc >= 2) {
 		target = argv[1];
 	}
 
 	Log(LogLevel::Debug, "Starting FuckMake");
+	printf("%s\n", argv[0]);
 
 	FuckMake fMake(argv[0], "Fuckfile", target);
 
